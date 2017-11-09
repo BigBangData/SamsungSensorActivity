889a723134bfa52b23fcec9b65e30edc27b69d72
Marcelo Sanches <30713252+BigBangData@users.noreply.github.com>
2017-11-09 01:13:53 -0700

    Update run_analysis2.r

--- run_analysis2.r
+++ run_analysis2.r
@@ -24,22 +24,12 @@ data_subset[, 2] <- activity_labels[data_subset[, 2], 2]
 
 ## 4. Appropriately labels the data set with descriptive variable names.
 x <- datanames[mean_features == TRUE]
-x <- sub("tBody", "TimeBody", x)
-x <- sub("tGravity", "TimeGravity", x)
-x <- sub("fBody", "FreqBody", x)
-x <- sub("BodyBody", "Body", x)
-x <- sub("-mean\\(\\)-X", "X.mean", x)
-x <- sub("-mean\\(\\)-Y", "Y.mean", x)
-x <- sub("-mean\\(\\)-Z", "Z.mean", x)
-x <- sub("-std\\(\\)-X", "X.sd", x)
-x <- sub("-std\\(\\)-Y", "Y.sd", x)
-x <- sub("-std\\(\\)-Z", "Z.sd", x)
-x <- sub("-meanFreq\\(\\)-X", "X.meanFreq", x)
-x <- sub("-meanFreq\\(\\)-Y", "Y.meanFreq", x)
-x <- sub("-meanFreq\\(\\)-Z", "Z.meanFreq", x)
-x <- sub("-mean\\(\\)", ".mean", x)
-x <- sub("-std\\(\\)", ".sd", x)
-x <- sub("-meanFreq\\(\\)", ".meanFreq", x)
+x <- sub("tBody", "TimeBody", x); x <- sub("tGravity", "TimeGravity", x); x <- sub("fBody", "FreqBody", x)
+x <- sub("BodyBody", "Body", x); x <- sub("-mean\\(\\)-X", "X.mean", x); x <- sub("-mean\\(\\)-Y", "Y.mean", x)
+x <- sub("-mean\\(\\)-Z", "Z.mean", x); x <- sub("-std\\(\\)-X", "X.sd", x); x <- sub("-std\\(\\)-Y", "Y.sd", x)
+x <- sub("-std\\(\\)-Z", "Z.sd", x); x <- sub("-meanFreq\\(\\)-X", "X.meanFreq", x)
+x <- sub("-meanFreq\\(\\)-Y", "Y.meanFreq", x); x <- sub("-meanFreq\\(\\)-Z", "Z.meanFreq", x)
+x <- sub("-mean\\(\\)", ".mean", x); x <- sub("-std\\(\\)", ".sd", x); x <- sub("-meanFreq\\(\\)", ".meanFreq", x)
 names(data_subset) <- x
 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
